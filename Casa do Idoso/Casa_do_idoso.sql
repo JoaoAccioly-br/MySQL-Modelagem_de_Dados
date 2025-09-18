e604624cfa2c18bc1b251f7b4bfe2a3b4bced5a5
João_Accioly <jguilhermeaccioly@gmail.com>
2025-09-18 19:27:03 -0300

    Atividades do dia 18/09

---
+++ Casa do Idoso/Casa_do_idoso.sql
@@ -0,0 +1,57 @@
+create database Casa_de_Repouso
+
+use Casa_de_Repouso
+
+create table idosos(
+Cod_idoso primary key,
+CPF varchar(14) unique not null,
+Nome varchar(100) not null,
+Cidade varchar(50) not null,
+Dt_nascimento date not null
+)
+
+create table responsaveis(
+CPF varchar(14) primary key,
+Nome varchar(100) not null,
+Telefone varchar(11) not null,
+Email varchar(60) not null,
+Endereço varchar(150) not null
+)
+
+create table medicos(
+Cod int primary key,
+Nome varchar(100) not null,
+Especializacao varchar(50) not null,
+Email varchar(60) not null,
+Telefone varchar(11) not null,
+Cidade varchar(50) not null
+)
+
+create table enfermeiros(
+Cod int primary key,
+Nome varchar(100) not null,
+Email varchar(60) not null,
+Telefone varchar(11) not null,
+Cidade varchar(50) not null
+)
+
+create table responsavel_idoso(
+Cpf_responsavel varchar(14) not null,
+Cod_idoso int not null
+)
+
+create table consulta_medico(
+Cod_medico int not null,
+Cod_idoso int not null,
+Dt_consulta date not null,
+Num_consulta int not null, // permite retornos do médico ao mesmo dia
+Observacoes varchar(255),
+
+)
+create table atendimento_enfermeiro(
+Cod_enfermeiro int not null,
+Cod_idoso int not null,
+Dt_atendimento date not null,
+Num_atendimento int not null,
+Observacoes varchar(255)
+)
---
+++ Estudo Filme/Estudo_filme.sql
@@ -0,0 +1,61 @@
+create database Estudo_Filme
+
+use Estudo_Filme
+
+create table Cliente(
+ID_cliente int primary key,
+Nome varchar(150) not null,
+Endereco varchar(255) not null
+)
+
+create table Produto(
+Cod_produto int primary key,
+Nome varchar(150) not null
+)
+
+create table Diretor(
+ID_diretor int primary key,
+Nome varchar(150) not null
+)
+
+create table Ator(
+Cod_ator int primary key,
+Nome varchar(155) not null,
+Foto_URL varchar(255) not null,
+Sexo varchar(1) not null,
+Dt_nascimento date not null,
+Cache decimal(8,2) not null
+)
+
+create table Cenario(
+ID_cenario int primary key,
+Local varchar(80) not null,
+Descricao varchar(255)
+)
+
+create table Filme(
+ID_filme int primary key,
+Nome varchar(155) not null,
+Duracao_min int not null,
+Dt_inicio date not null,
+Dt_fim date not null,
+ID_cliente int not null,
+ID_diretor int not null,
+Cod_produto int not null
+)
+
+create table Locacao(
+ID_locacao int primary key,
+ID_filme int not null,
+ID_cenario int not null,
+Dt_inicio date not null,
+Dt_fim date not null,
+Valor_gasto decimal(8,2) not null
+)
+
+create table Atuacao(
+Cod_atuacao int primary key,
+Cod_ator int not null,
+ID_filme int not null,
+Personagem varchar(155) not null
+)
