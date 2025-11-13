5088af922a318b089cde9cd30aa8e83dfbd280c8
João_Accioly <jguilhermeaccioly@gmail.com>
2025-11-13 14:07:36 -0300

    Update CINEMA_DB.sql

--- Banco da dados/Exercícios/CINEMA_DB.sql
+++ Banco da dados/Exercícios/CINEMA_DB.sql
@@ -1,11 +1,36 @@
 CREATE DATABASE IF NOT EXISTS CINEMA_DB;
 USE CINEMA_DB;
 
-CREATE TABLE FILMES (
-    ID_FILME INT AUTO_INCREMENT PRIMARY KEY,
-    TITULO VARCHAR(100),
-    DIRETORES VARCHAR(100),
-    ATORES VARCHAR(200),
-    GENEROS VARCHAR(100),
-    PRODUTORA VARCHAR(50)
+--CREATE TABLE FILMES (
+--    ID_FILME INT AUTO_INCREMENT PRIMARY KEY,
+--    TITULO VARCHAR(100),
+--    DIRETORES VARCHAR(100),
+--    ATORES VARCHAR(200),
+--    GENEROS VARCHAR(100),
+--    PRODUTORA VARCHAR(50)
+--);
+
+CREATE TABLE diretores(
+    id_diretor int auto_increment primary key,
+    nome varchar(100)
 );
+
+CREATE TABLE atores(
+    id_ator int auto_increment primary key,
+    nome varchar(100)
+);
+
+CREATE TABLE generos(
+    id_genero int auto_increment primary key,
+    nome varchar(50)
+);
+
+CREATE TABLE produtoras(
+    id_produtora int auto_increment primary key,
+    nome varchar(100)
+);
+
+CREATE TABLE filmes(
+    id_filme int auto_increment primary key,
+    titulo varchar(100)
+)
